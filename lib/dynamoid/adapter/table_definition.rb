683a13e1d093ab0fa3d22ecf5b584b5ce6e15210
Sebastian Oelke <soelke@babbel.com>
2015-07-16 16:42:23 +0200

    Refactoring.

--- lib/dynamoid/adapter/table_definition.rb
+++ lib/dynamoid/adapter/table_definition.rb
@@ -25,17 +25,31 @@ module Dynamoid
       private
 
       def key_schema
+        elements_for :key_schema
+      end
+
+      def attribute_definitions
+        elements_for :attribute_definitions
+      end
+
+      def elements_for(type)
         hash_key, range_key = @options.values_at(:hash_key, :range_key)
 
         schema = []
-        schema << key_schema_element(hash_key,  :hash)
-        schema << key_schema_element(range_key, :range) if range_key
+
+        if type == :key_schema
+          schema << key_schema_element(hash_key,  :hash)
+          schema << key_schema_element(range_key, :range) if range_key
+        elsif type == :attribute_definitions
+          schema << attribute_definitions_element(hash_key,  :hash)
+          schema << attribute_definitions_element(range_key, :range) if range_key
+        end
 
         schema
       end
 
       def key_schema_element(desc, key_type)
-        (name, type) = desc.to_a.first
+        name = desc.keys.first
 
         {
           attribute_name: name.to_s,
@@ -43,24 +57,11 @@ module Dynamoid
         }
       end
 
-      def attribute_definitions
-        hash_key, range_key = @options.values_at(:hash_key, :range_key)
-
-        # default options for :hash_key
-        hash_key ||= { :id => :string }
-
-        schema = []
-        schema << schema_element(hash_key,  :hash)
-        schema << schema_element(range_key, :range) if range_key
-
-        schema
-      end
-
-      def schema_element(desc, key_type)
+      def attribute_definitions_element(desc, key_type)
         (name, type) = desc.to_a.first
 
         unless [:string, :number, :binary].include?(type)
-          msg = "invalid #{key_type} key type, expected :string, :number or :binary"
+          msg = "Invalid #{key_type} key type, expected :string, :number or :binary."
           raise ArgumentError, msg
         end
 
